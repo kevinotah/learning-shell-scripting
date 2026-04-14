6076c3af9dd781b7eac8cba1b8b4cc2ae2fb28fe
Kevin Otah <otahkevin@gmail.com>
2026-04-14 02:09:07 +0200

    comments and variables

--- 00-intro.sh
+++ 00-intro.sh
@@ -1,1 +1,28 @@
-Intro
+#! /usr/bin/bash
+
+echo "Hello World!"
+
+array=(PROGRAMMING IS FUN)
+
+echo 
+for word in "${array[@]}"; do echo $word; done
+echo
+
+# comments
+# this is a comment
+echo "What is love? Baby don't hurt me." # this is also a comment
+echo
+
+# (some) system variables
+echo $BASH
+echo $BASH_VERSION
+echo $HOME
+echo $PWD
+echo
+
+# user variables
+name=Kevin age=24 country=Nigeria
+echo "My name is $name"
+printf "My name is $name, I'm $age years old and I come from $country.\n"
+echo
+
